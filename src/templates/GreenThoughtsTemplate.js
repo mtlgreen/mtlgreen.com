f040360e2b9871e6278258c614204a3dd844e285
greatwillow <gregory.g.denys@gmail.com>
2018-11-11 16:51:49 -0500

    modify layout and styling of green-thoughts template while adding author and date info

--- src/templates/GreenThoughtsTemplate.js
+++ src/templates/GreenThoughtsTemplate.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import CommonPageHeadTitle from '../components/CommonPageHeadTitle/CommonPageHeadTitle'
 import Img from 'gatsby-image'
+import getFormattedDate from '../utils/getFormattedDate'
 
 import './greenThoughtsTemplate.scss'
 
 class GreenThoughtsTemplate extends Component {
+  constructor() {
+    super()
+    this.state = {
+      formattedDate: null,
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      formattedDate: getFormattedDate(
+        this.props.data.markdownRemark.frontmatter.date
+      ),
+    })
+  }
+
   render() {
     const post = this.props.data.markdownRemark
     const projectImages = this.props.data.markdownRemark.frontmatter.images
+    const date = this.state.formattedDate
 
     return (
-      <div className="green-thoughts-template__body__container">
+      <div className="green-thoughts-template__container">
         <Img
           style={{
             width: '100vw',
+            maxWidth: '700px',
             height: '65vh',
             marginTop: '80px',
           }}
@@ -23,15 +41,46 @@ class GreenThoughtsTemplate extends Component {
             aspectRatio: 1 / 1,
           }}
         />
-        <div className="green-thoughts-template__article-title">
-          {post.frontmatter.title}
+        <div className="green-thoughts-template__body">
+          <div className="green-thoughts-template__article-title">
+            {post.frontmatter.title}
+          </div>
+          <div className="green-thoughts-template__article-description">
+            {post.frontmatter.shortDescription}
+          </div>
+          <div className="green-thoughts-template__author-info__container">
+            <Img
+              style={{
+                width: '80px',
+                height: '80px',
+                borderRadius: '100%',
+              }}
+              sizes={{
+                ...post.frontmatter.thumbnail.childImageSharp.sizes,
+                aspectRatio: 1 / 1,
+              }}
+            />
+            <div className="green-thoughts-template__author-name-date">
+              <div>{post.frontmatter.authorName}</div>
+              <div>
+                {date &&
+                  date.weekDay +
+                    ' ' +
+                    date.day +
+                    ' ' +
+                    date.monthLong +
+                    ' ' +
+                    date.year}
+              </div>
+            </div>
+          </div>
+          <div
+            className="green-thoughts-template__article-excerpt__container"
+            dangerouslySetInnerHTML={{
+              __html: post.html,
+            }}
+          />
         </div>
-        <div
-          className="green-thoughts-template__article-excerpt__container"
-          dangerouslySetInnerHTML={{
-            __html: post.html,
-          }}
-        />
       </div>
     )
   }
@@ -45,6 +94,16 @@ export const greenThoughtsTemplateQuery = graphql`
       html
       frontmatter {
         title
+        shortDescription
+        authorImage {
+          childImageSharp {
+            sizes(maxWidth: 200) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
+        authorName
+        date
         # images {
         #   childImageSharp {
         #     sizes(maxWidth: 1000) {
